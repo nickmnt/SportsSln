5ca66448ccd86d202b45db1903fc307cd75ee121
Nima Moein <nimamt@users.noreply.github.com>
2021-03-24 23:51:02 +0430

    Created a unit test.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers {
     public class HomeController : Controller {
-        public IActionResult Index() => View();
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo) {
+            repository = repo;
+        }
+        
+        public IActionResult Index() => View(repository.Products);
 
     }
 }
