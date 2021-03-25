eaf77e3ae29122f163b48a402622d67c6ce87414
Nima Moein <nimamt@users.noreply.github.com>
2021-03-25 16:24:17 +0430

    Added a view component etc.

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components {
+    public class NavigationMenuViewComponent: ViewComponent {
+        public string Invoke() {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
