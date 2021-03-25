ec6645c021710d239739ffbb506dcee87da4d48e
Nima Moein <nimamt@users.noreply.github.com>
2021-03-25 23:49:10 +0430

    Updated the database.

--- SportsStore/Models/StoreDbContext.cs
+++ SportsStore/Models/StoreDbContext.cs
@@ -9,5 +9,7 @@ namespace SportsStore.Models {
         public StoreDbContext(DbContextOptions<StoreDbContext> options)
             : base(options) { }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
     }
 }
